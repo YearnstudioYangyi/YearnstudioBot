281a1a362920b70f0d3802762a9d8d632a6be917
YearnstudioYangyi <yangyi@yearnstudio.cn>
2024-12-24 17:35:20 +0800

    修改RCON端口号

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 from rcon import Client # type: ignore
 
 # RCON服务器地址和端口
-rcon_host = 'localhost'  # 替换为你的服务器地址
-rcon_port = 25567
+rcon_host = '127.0.0.1'  # 替换为你的服务器地址
+rcon_port = 25575
 rcon_password = '@Fkchh000'
 
 # 创建RCON客户端
