3aa3c0e6ce23ab8955bd8abab66216f920bd00e4
YearnstudioYangyi <yangyi@yearnstudio.cn>
2024-12-25 06:28:26 +0800

    启动循环，捕获Ctrl+C

--- Main.py
+++ Main.py
@@ -1,12 +1,19 @@
 from rcon import Client # type: ignore
 
 # RCON服务器地址和端口
-rcon_host = '127.0.0.1'  # 替换为你的服务器地址
+rcon_host = '127.0.0.1'
 rcon_port = 25575
 rcon_password = '@Fkchh000'
 
-# 创建RCON客户端
-with Client(rcon_host, rcon_port, passwd=rcon_password) as client:
-    # 发送命令到服务器
-    response = client.run('list')  # 你可以替换为任何你想要执行的命令
-    print(response)
+def Run(command,c):
+    response = c.run(command)
+    return response
+
+if __name__ == '__main__':
+    with Client(rcon_host, rcon_port, passwd=rcon_password) as c:
+        while True:
+            try:
+                print(Run(input('请输入命令：'), c))
+            except KeyboardInterrupt:
+                print('\n退出')
+                exit()
