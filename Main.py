bdf6e1c8abab74fa81035dafff2f14c700ca6441
yangyi <3303289608@qq.com>
2025-01-29 19:31:57 +0800

    更新为幻梦乡框架

--- Main.py
+++ Main.py
@@ -1,19 +1,97 @@
+from datetime import datetime, timedelta
+from email.mime.text import MIMEText
+import hashlib
+import random
+import re
+import smtplib
+import subprocess
+from waitress import serve # type: ignore
+from flask import Flask, jsonify,request # type: ignore
+from flask_cors import CORS # type: ignore
+from wxpusher import WxPusher # type: ignore
+import requests # type: ignore
+import pymysql # type: ignore
+import time
+import os
+import json
+import shutil
+import psutil # type: ignore
+from os.path import join
+import importlib.util
+import string
 from rcon import Client # type: ignore
 
-# RCON服务器地址和端口
+app = Flask(__name__)
+
+help_text = """/mc [指令] 执行服务器指令
+/help 获取帮助
+/status [条目] 查询服务器状态
+ 可用条目：
+    运行内存：查询服务器总内存
+    CPU：查询服务器CPU占用率
+    运行时长：查询服务器运行时长
+"""
 rcon_host = '127.0.0.1'
 rcon_port = 25575
 rcon_password = '@Fkchh000'
+api_ip = 'http://127.0.0.1:3000'
 
-def Run(command,c):
-    response = c.run(command)
-    return response
+def GetMemberRole(data):
+    id = data['user_id']
 
-if __name__ == '__main__':
-    with Client(rcon_host, rcon_port, passwd=rcon_password) as c:
-        while True:
-            try:
-                print(Run(input('请输入命令：'), c))
-            except KeyboardInterrupt:
-                print('\n退出')
-                exit()
+def SendGroupMsg(data,msg):
+    try:
+        requests.get(f"{api_ip}/send_group_msg?group_id={data['group_id']}&msg_id={data['message_id']}&message={msg}&auto_escape=false")
+    except:
+        print("发送失败:" + f"{api_ip}/send_group_msg?group_id={data['group_id']}&msg_id={data['message_id']}&message={msg}&auto_escape=false")
+
+def SendPrivateMsg(data,msg):
+    try:
+        requests.get(f"{api_ip}/send_private_msg?user_id={data['user_id']}&message={msg}&auto_escape=false")
+    except:
+        print("发送失败:" + f"{api_ip}/send_private_msg?user_id={data['user_id']}&message={msg}&auto_escape=false")
+
+def get_cpu_usage():
+    """ 获取当前系统的CPU使用率 """
+    cpu_percent = psutil.cpu_percent(interval=1)
+    return cpu_percent
+
+def GetMemAll():
+    mem = psutil.virtual_memory()
+    return round(mem.total / (1024.0 ** 3),2)
+
+@app.route("/",methods=["POST","GET"])
+def root():
+    data = request.json
+    if 'group_id' not in data:
+        SendPrivateMsg(data,"请使用群聊发送命令")
+    print(data)
+    msg = str(data['raw_message'])
+    if msg.startswith("/status "):
+        msg = msg.replace("/status ","")
+        if msg == "运行内存":
+            mem = GetMemAll()
+            SendGroupMsg(data,f"总内存为{mem}GB")
+        elif msg == "CPU":
+            cpu = get_cpu_usage()
+            SendGroupMsg(data,f"CPU占用为{cpu}%")
+        elif msg == "运行时长":
+            # 获取系统启动时间
+            boot_time = datetime.fromtimestamp(psutil.boot_time())
+            now = datetime.now()
+            uptime = now - boot_time
+            SendGroupMsg(data,f"系统运行时长为{uptime}")
+        else:
+            SendGroupMsg(data,"请输入正确的参数")
+    elif msg.startswith("/mc "):
+        # 去掉开头的/mc
+        msg = msg.replace("/mc ","")
+        with Client(rcon_host, rcon_port, passwd=rcon_password) as c:
+            response = c.run(msg)
+            SendGroupMsg(data,response)
+    elif msg == "/help":
+        SendGroupMsg(data,help_text)
+    return {}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8090, debug=True, threaded=True)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # btr-bot
-方块茶话会官方框架机器人，基于mcpi+Python
+方块茶话会官方框架机器人，基于Gensokyo(幻想乡)框架、OneBot11标准开发
+***
+## 指令列表
+/help 帮助
+/mc 执行MC指令(仅管理员可用)
+/status 获取服务器状态
